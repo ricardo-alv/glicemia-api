ec8090e4c97a574667b0c43926cb6266ba2b8803
Ricardo Alves <ricardoalv.88@gmail.com>
2025-04-05 17:29:27 -0300

    criando pdf

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
    return 'Success';
@@ -13,4 +14,13 @@ Route::get('/reset-password', function () {
 })->name('password.request');
 
 Route::post('/password-update', [ResetPasswordController::class, 'update'])->name('password.update');
+
+
+Route::get('/teste-pdf', function () {
+    $data = ['titulo' => 'PDF gerado com sucesso!'];
+
+    $pdf = Pdf::loadView('exemplo', $data);
+
+    return $pdf->stream('teste.pdf');
+});
 
