b4dfb21f952497e565c6dfe272be6f9c6b4acb96
Ricardo Alves <ricardoalv.88@gmail.com>
2025-04-05 17:30:55 -0300

    remove pdf

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Api\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
-use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
    return 'Success';
@@ -14,13 +13,4 @@ Route::get('/reset-password', function () {
 })->name('password.request');
 
 Route::post('/password-update', [ResetPasswordController::class, 'update'])->name('password.update');
-
-
-Route::get('/teste-pdf', function () {
-    $data = ['titulo' => 'PDF gerado com sucesso!'];
-
-    $pdf = Pdf::loadView('exemplo', $data);
-
-    return $pdf->stream('teste.pdf');
-});
 
