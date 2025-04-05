997c2a06da071a6167aa480805f712619d665fc3
Ricardo Alves <ricardoalv.88@gmail.com>
2025-04-05 16:25:54 -0300

    CREATE EXTENSION IF NOT EXISTS

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {           
+        DB::statement('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+        
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary()->default(DB::raw('uuid_generate_v4()')); //postegres
             $table->string('name');
