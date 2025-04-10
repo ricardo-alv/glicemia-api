ac13a7ac1ba48d13965afb82b170791b6479028b
Ricardo Alves <ricardoalv.88@gmail.com>
2025-04-10 12:57:14 -0300

    chore:insert glucose_3morning in resource

--- app/Http/Resources/GlucoseResource.php
+++ app/Http/Resources/GlucoseResource.php
@@ -20,6 +20,7 @@ class GlucoseResource extends JsonResource
             "ultra_fast_insulin" => $this->ultra_fast_insulin,
             "carbs" => $this->carbs,
             "after_glucose" => $this->after_glucose,
+            "glucose_3morning" => $this->glucose_3morning,
             "meal_type" => $this->mealType->name,     
             "meal_type_id" =>  $this->mealType->id,   
         ];
